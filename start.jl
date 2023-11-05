b0153bd87fc2d2a58a6b462d057e05f41bba5f8d
Junsen Wang <jsw@ucas.ac.cn>
2023-11-05 14:15:18 +0800

    fix a bug for  function

--- start.jl
+++ start.jl
@@ -1,7 +1,7 @@
 using Pkg
 path_to_SIEGE = "Path/To/SIEGE"
-project_folder_name = "project_folder_name"
+project_folder_name = "ssm_hz"
 Pkg.activate(temp=true)
 Pkg.develop(path=path_to_SIEGE)
 using SIEGE
-main(project_folder_name, path=path_to_SIEGE)
+main(project_folder_name, path_to_SIEGE)
